bb7c6d12c997e99456002c20a2db955ec39e8ffd
Adam Grajdek <avi19990@gmail.com>
2021-12-05 23:11:54 +0100

    Task 5 setup

---
+++ src/Main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include "Task5.h"
+
+int main()
+{
+	int taskToSolve = 5;
+
+	switch (taskToSolve)
+	{
+	case 5:
+		std::cout << "Solution to task " << taskToSolve << ": " << Task5() << std::endl;
+		break;
+	}
+}
---
+++ src/Task5.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <vector>
+#include <fstream>
+
+struct Vent
+{
+	int x1, y1;
+	int x2, y2;
+};
+
+std::vector<Vent> readTask5Data()
+{
+	return std::vector<Vent>();
+}
+
+int Task5()
+{
+	return 0;
+}
